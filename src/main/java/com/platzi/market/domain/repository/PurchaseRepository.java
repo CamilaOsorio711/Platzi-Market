e5806ba56dadc2b724d816a5deb92332c91e4f80
Camila Osorio <camik_711@hotmail.com>
2021-09-10 14:08:52 -0500

    API Terminada

---
+++ src/main/java/com/platzi/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,14 @@
+package com.platzi.market.domain.repository;
+
+import com.platzi.market.domain.Purchase;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
